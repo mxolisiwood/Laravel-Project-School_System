d8ab7806845ceeadab65bda83dc06341550299ca
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-04-08 13:36:05 +0200

    Bache.php file added

---
+++ app/Models/Batche.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Model;
+
+/**
+ * Class Batche
+ * @package App\Models
+ * @version April 8, 2020, 11:32 am UTC
+ *
+ * @property integer batch
+ */
+class Batche extends Model
+{
+
+    public $table = 'batches';
+    
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+
+
+
+    public $fillable = [
+        'batch'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'batch_id' => 'integer',
+        'batch' => 'integer'
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'batch' => 'required'
+    ];
+
+    
+}
