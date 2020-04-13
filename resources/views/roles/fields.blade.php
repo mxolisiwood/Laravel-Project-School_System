a917f19f975b3f803f1a26a8ed32c3aaa9abc714
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-04-13 16:56:01 +0200

    Changed roles fields and index

--- resources/views/roles/fields.blade.php
+++ resources/views/roles/fields.blade.php
@@ -1,11 +1,36 @@
+<!-- Button trigger modal -->
+
+<!------------------------modal----------------->
+
+<div class="modal fade" id="role-add-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
 <!-- Name Field -->
-<div class="form-group col-sm-6">
+<div class="form-group">
     {!! Form::label('name', 'Name:') !!}
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
-<!-- Submit Field -->
+<!-- Submit Field --
 <div class="form-group col-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
     <a href="{{ route('roles.index') }}" class="btn btn-default">Cancel</a>
+</div>-->
+
+</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        {!! Form::submit('Create Role', ['class' => 'btn-success']) !!}
+      
+      </div>
+    </div>
+  </div>
 </div>
