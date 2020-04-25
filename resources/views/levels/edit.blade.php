e49e85b9b6a6b08dd89295ed5cd40a2faf0bd905
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-04-25 13:16:20 +0200

    Level CRUD

--- resources/views/levels/edit.blade.php
+++ resources/views/levels/edit.blade.php
@@ -13,7 +13,30 @@
                <div class="row">
                    {!! Form::model($level, ['route' => ['levels.update', $level->id], 'method' => 'patch']) !!}
 
-                        @include('levels.fields')
+                                        <!-- Level Field -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('level', 'Level:') !!}
+                        {!! Form::text('level', null, ['class' => 'form-control']) !!}
+                    </div>
+
+                    <!-- Course Id Field -->
+                    <div class="form-group col-sm-6">
+                        {!! Form::label('course_id', 'Course Id:') !!}
+                        {!! Form::number('course_id', null, ['class' => 'form-control']) !!}
+                    </div>
+
+                    <!-- Level Description Field -->
+                    <div class="form-group col-sm-12 col-lg-12">
+                        {!! Form::label('level_description', 'Level Description:') !!}
+                        {!! Form::textarea('level_description', null, ['class' => 'form-control']) !!}
+                    </div>
+
+                    <!-- Submit Field -->
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+                        {!! Form::submit('Update Level', ['class' => 'btn btn-primary']) !!}
+                    <!-- <a href="{{ route('batches.index') }}" class="btn btn-default">Cancel</a>-->
+                    </div>
 
                    {!! Form::close() !!}
                </div>
