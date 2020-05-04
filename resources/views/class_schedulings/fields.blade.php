7b8430e749051fde437348e2945ab730355e377a
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-05-04 06:46:56 +0200

    level id changed fromid to level_id

--- resources/views/class_schedulings/fields.blade.php
+++ resources/views/class_schedulings/fields.blade.php
@@ -35,7 +35,7 @@ aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 
 <!-- Level Id Field -->
 <div class="form-group col-sm-6">
-    <select class="form-control" name="id" id="level_id">
+    <select class="form-control" name="level_id" id="level_id">
         <option value ="">Select Level</option>
         @foreach($level as $lev)
         <option value="{{$lev->level_id}}">{{$lev->level}}</option>
