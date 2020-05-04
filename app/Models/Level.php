10beccca29659a28001c0e91a92eefd02f043dba
Mxolisi Silabela <silabelamxolisi@gmail.com>
2020-05-04 06:45:07 +0200

    primarykey on level models

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -23,7 +23,7 @@ class Level extends Model
 
 
 
-
+   protected $primaryKey = 'level_id';
     public $fillable = [
         'level',
         'course_id',
@@ -36,7 +36,7 @@ class Level extends Model
      * @var array
      */
     protected $casts = [
-        'id' => 'integer',
+        'level_id' => 'integer',
         'level' => 'string',
         'course_id' => 'integer',
         'level_description' => 'string'
